3a4e26d8dc606f35cbf94ec8749e6bd083d96122
andi <andreas-abel_student2022@wilder.school>
2022-05-05 13:50:37 +0200

    init

---
+++ src/main/java/com/WildCodeSchool/JavaJDBCintroduction/JavaJdbCintroductionApplication.java
@@ -0,0 +1,13 @@
+package com.WildCodeSchool.JavaJDBCintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJdbCintroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJdbCintroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WildCodeSchool/JavaJDBCintroduction/JavaJdbCintroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WildCodeSchool.JavaJDBCintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJdbCintroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
